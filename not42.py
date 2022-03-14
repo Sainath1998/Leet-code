7af2804b104d34532e25ee7bf28457b4971ef668
Sainath <97929522+Sainath1998@users.noreply.github.com>
2022-03-14 08:55:14 +0530

    Add files via upload

---
+++ not42.py
@@ -0,0 +1,10 @@
+# cook your dish here
+num = 0
+other_nums = []
+while num != 42:
+    num = int(input(""))
+    if num != 42:
+        other_nums.append(num)
+    
+for n in other_nums:
+    print(n)
